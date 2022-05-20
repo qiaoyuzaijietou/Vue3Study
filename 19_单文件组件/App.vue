1b2b8e32708730f8bf28f834c68b47c76ad9ec4e
qiaoyu <1903293766@qq.com>
2022-05-20 23:34:52 +0800

    vue学习

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <School></School>
+        <Student></Student>
+    </div>
+</template>
+
+<script>
+// 引入组件
+import School from './School'
+import Student from './Student'
+import Student1 from './Student.vue';
+export default {
+    name:'App',
+    components:{
+    School,
+    Student,
+    Student1
+}
+};
+</script>
---
+++ 19_单文件组件/main.js
@@ -0,0 +1,7 @@
+import App from './App.vue'
+
+/* 新建一个Vue实例 */
+new Vue({
+        el: '#root',
+        components:{App},
+    });
